7f7a90f2be110670940e6fb3039ccf07e0a06d21
shubhamgargBG <shubham.garg@blueguardian.ca>
2024-05-13 12:15:37 -0700

    implement right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,24 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = {firstName: 'Shubham'};
+  const loggedIn = {
+    firstName: "Shubham",
+    lastName: "Garg",
+    email: "shubhamgargasu@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
-          <HeaderBox 
+          <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transaction efficiently"
-          /> 
+          />
 
           <TotalBalanceBox
             accounts={[]}
@@ -21,9 +26,12 @@ const Home = () => {
             totalCurrentBalance={1250.99}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
     </section>
-  )
+  );
 };
 
 export default Home;
